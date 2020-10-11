118002168c317cde8dc38a5e2e939977cfe642e0
Justin Carter <justin.carter@censornet.com>
2020-10-11 12:14:03 +0100

    Added additional remote debug

--- routes/events/modules/message.js
+++ routes/events/modules/message.js
@@ -4,6 +4,7 @@ const database = require('../../../db/database')
 
 module.exports = {
     post: async (args) => {
+        console.log(args);
         const result = await axios.post(`${process.env.API_URL}/chat.postMessage`, qs.stringify(args));
         try {
             if (result.data.ok === false){
